d64ed9f39705b6acb7ff932ead158f9380d11111
and-edcox <edward.cox@and.digital>
2022-09-19 14:32:36 +0100

    updated board vis

---  chess.py
+++  chess.py
@@ -6,22 +6,24 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import os
 
+
 class Board:
-  def __init__(self) -> None:
-    #  black = 0, white = 1
-    self.board = []
+    def __init__(self) -> None:
+        self.board = []
 
-    for i in range(8):
-      self.board.append([(x+i)%2 for x in range(8)])
+        for i in range(8):
+            self.board.append([(x + i + 1) % 2 for x in range(8)])
 
-  def plot_board(self):
+    def plot_board(self):
 
         plt.matshow(self.board, cmap=ListedColormap(["k", "w"]))
         ax = plt.gca()
-        ax.set_xticks([x - 0.5 for x in range(1, 8)], minor=True)
-        ax.set_yticks([y - 0.5 for y in range(1, 8)], minor=True)
+        ax.set_xticks([x - 0.5 for x in range(1, 9)], minor=True)
+        ax.set_yticks([y - 0.5 for y in range(1, 9)], minor=True)
+        ax.set_xticklabels([0, "A", "B", "C", "D", "E", "F", "G", "H"])
+        ax.set_yticklabels([0, 8, 7, 6, 5, 4, 3, 2, 1])
         plt.grid(which="minor", ls="-", lw=2)
-        plt.grid(c="k", lw="2", which="minor")
+        plt.grid(c="k", lw="0", which="minor")
 
         if not os.path.exists("static/images"):
             os.mkdir("static/images")
